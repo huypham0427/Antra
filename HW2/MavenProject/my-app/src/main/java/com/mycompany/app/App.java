ffb27a22c2ab2461610fef70fd6d49d897919720
huypham0427 <59068418+huypham0427@users.noreply.github.com>
2022-08-21 14:57:07 -0700

    Add files via upload

---
+++ HW2/MavenProject/my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+/**
+ * Hello world with Hibernate!
+ *
+ * Fix to run Maven project
+ * https://stackoverflow.com/questions/4904052/what-does-this-symbol-mean-in-intellij-red-circle-on-bottom-left-corner-of-fil
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World with Hibernate!" );
+    }
+}
