a00f069d98062e65ea962391a3fb6b234728bd4c
huypham0427 <59068418+huypham0427@users.noreply.github.com>
2022-08-25 22:18:48 -0700

    Delete HW4 directory

--- HW4/java/com/example/demo4/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo4.repository;
-
-import com.example.demo4.domain.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
-}
